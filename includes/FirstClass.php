1b233de78ce358388b7331ef12a31157f2487121
Rodrigo Lopez <scdtp2019@gmail.com>
2023-04-19 20:18:32 +0800

    add exmaple of a class and object

---
+++ includes/FirstClass.php
@@ -0,0 +1,6 @@
+<?php 
+
+class FirstClass{
+    // Properties and  Methods
+    public $information = "This is an example of a class that contain some information!";
+}
---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Object Oriented Programming - PHP</title>
+</head>
+<body>
+
+<h1>OOP - Object Oriented Programming with PHP</h1>
+
+<p> Compared to other OOP languages (Object oriented programming) PHP is much easier compared
+to some programming languages. </p>
+
+<p> C#, Python, Java and Ruby</p>
+
+<p>PHP5 and higher </p>
+
+
+<h2>WHY USE OOP? </h2>
+<ul>
+    <li>OOP is faster and easier to execute</li>
+    <li>Providing a clear structure for your programs</li>
+    <li>Practice DRY "Don't Repeat Yourself", we write code that is a lot easier to maintain and debug</li>
+    <li>Create Reusable applications</li>
+    <li>Companies want OOP</li>
+    <li>Better for group work!</li>
+    <li>Better organize or code</li>
+    <li>Medium - Large websites</li>
+</ul>
+
+<hr>
+
+<h2>MVC MODEL</h2>
+<h3>TASKS OF PHP</h3>
+<ol>
+    <li>Query and connect to db.</li>
+    <li>Get input from users.</li>
+    <li>Display things on the website.</li>
+</ol>
+
+
+<?php 
+   class Fruit{
+        //Properties
+        public $name;
+        public $color;
+
+
+        //Methods
+        function setName($name){
+            $this->name= $name;
+        }
+
+        function getName(){   
+            return $this->name;
+        }
+   }
+
+   $apple = new Fruit();
+   $banana = new Fruit();
+   $apple->setName('Apple');
+   $banana->setName('Banana');
+
+   echo $apple->getName();
+   echo "<br>";
+   echo $banana->getName();
+
+   // Call using browser when the user clicks on something
+   // Call or invoke a funciton inside of our script or code 
+?>
+<!-- 
+<script>
+    setTimeout(() => {
+        
+    }, timeout);
+</script> -->
+
+
+
+</body>
+</html>
