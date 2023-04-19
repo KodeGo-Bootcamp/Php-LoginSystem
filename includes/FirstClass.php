b41c51b58ae5502000a69c72c998bf1b3944f478
Rodrigo Lopez <scdtp2019@gmail.com>
2023-04-19 20:32:38 +0800

    Add coding challenge

--- includes/FirstClass.php
+++ includes/FirstClass.php
@@ -8,5 +8,13 @@ class FirstClass{
 
 }
 
+//Don't change anything above here...
 
+//CODING CHALLENGE:
+
+// Create an Object 
+// Add your code here....
+
+// Pass it in as an argument inside of the var_dump() built-in function in the PHP language
+// Add your code here..
 var_dump();
