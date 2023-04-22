ea820734e8958699c38095f5b1b4c8702b284779
aeuhim <ericmichaelbedanaconstantino@gmail.com>
2023-04-22 11:38:59 +0800

    Add person object

--- includes/Fruit.php
+++ includes/Fruit.php
@@ -1,13 +1,19 @@
 <?php
 
 // create a class FRUIT
-class Fruit {
-// create a public property name
+class Fruit
+{
+    // create a public property name
     public $name = "apple";
-// create a  private property color
+    // create a  private property color
     private $color = "red";
-// create a protected property weight
+    // create a protected property weight
     protected $weight = "1kg";
+
+    // function __construct($name)
+    // {
+    //     $this->name = $name;
+    // }
 }
 
 
@@ -15,19 +21,17 @@ class Fruit {
 
 
 // create a class Person
-class Person extends Fruit {
-// create a property for protected person name
+class Person extends Fruit
+{
+    // create a property for protected person name
     protected $person = "Jerome";
 
-// create a method eatFruit
-    public function eatFruit(){
+    // create a method eatFruit
+    public function eatFruit()
+    {
         $a = "Hi I am" . $this->person . "and my favorite fruit is" . $this->name . ".";
         return $a;
     }
-   
-// declare and return a declared and initialzed variable $eat with the strings "Hi I am" <personanme> and my favorite fruit is <fruitname>.
+
+    // declare and return a declared and initialzed variable $eat with the strings "Hi I am" <personanme> and my favorite fruit is <fruitname>.
 }
-
-
-
-
--- index.php
+++ index.php
@@ -1,135 +1,143 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Object Oriented Programming - PHP</title>
 </head>
+
 <body>
 
-<h1>OOP - Object Oriented Programming with PHP</h1>
+    <h1>OOP - Object Oriented Programming with PHP</h1>
 
-<p> Compared to other OOP languages (Object oriented programming) PHP is much easier compared
-to some programming languages. </p>
+    <p> Compared to other OOP languages (Object oriented programming) PHP is much easier compared
+        to some programming languages. </p>
 
-<p> C#, Python, Java and Ruby</p>
+    <p> C#, Python, Java and Ruby</p>
 
-<p>PHP5 and higher </p>
-
-
-<h2>WHY USE OOP? </h2>
-<ul>
-    <li>OOP is faster and easier to execute</li>
-    <li>Providing a clear structure for your programs</li>
-    <li>Practice DRY "Don't Repeat Yourself", we write code that is a lot easier to maintain and debug</li>
-    <li>Create Reusable applications</li>
-    <li>Companies want OOP</li>
-    <li>Better for group work!</li>
-    <li>Better organize or code</li>
-    <li>Medium - Large websites</li>
-</ul>
-
-<hr>
-
-<h2>MVC MODEL</h2>
-<h3>TASKS OF PHP</h3>
-<ol>
-    <li>Query and connect to db.</li>
-    <li>Get input from users.</li>
-    <li>Display things on the website.</li>
-</ol>
+    <p>PHP5 and higher </p>
 
 
+    <h2>WHY USE OOP? </h2>
+    <ul>
+        <li>OOP is faster and easier to execute</li>
+        <li>Providing a clear structure for your programs</li>
+        <li>Practice DRY "Don't Repeat Yourself", we write code that is a lot easier to maintain and debug</li>
+        <li>Create Reusable applications</li>
+        <li>Companies want OOP</li>
+        <li>Better for group work!</li>
+        <li>Better organize or code</li>
+        <li>Medium - Large websites</li>
+    </ul>
 
-<!-- 
+    <hr>
+
+    <h2>MVC MODEL</h2>
+    <h3>TASKS OF PHP</h3>
+    <ol>
+        <li>Query and connect to db.</li>
+        <li>Get input from users.</li>
+        <li>Display things on the website.</li>
+    </ol>
+
+
+
+    <!-- 
 <script>
     setTimeout(() => {
         
     }, timeout);
 </script> -->
 
-<?php
-echo "<hr>";
-echo "<h2>FirstClass Example</h2>";
-echo "<br>";
-include_once 'includes/FirstClass.php';
-?>
+    <?php
+    echo "<hr>";
+    echo "<h2>FirstClass Example</h2>";
+    echo "<br>";
+    include_once 'includes/FirstClass.php';
+    ?>
 
 
-<hr>
+    <hr>
 
-<h2>CONSTRUCTS</h2>
+    <h2>CONSTRUCTS</h2>
 
-<?php 
-   class Fruit{
+    <?php
+    class Fruit
+    {
         //Properties
         public $name;
         public $color;
 
 
         //Methods
-        function __construct($name){
-            $this->name= $name;
+        function __construct($name)
+        {
+            $this->name = $name;
         }
 
-        function getName(){   
+        function getName()
+        {
             return $this->name;
         }
-   }
+    }
 
-   $apple = new Fruit('Apple');
-   $banana = new Fruit("Banana");
-   
-   echo $apple->getName();
-   echo "<br>";
-   echo $banana->getName();
-   // Call using browser when the user clicks on something
-   // Call or invoke a funciton inside of our script or code 
-?>
+    $apple = new Fruit('Apple');
+    $banana = new Fruit("Banana");
 
-
-<h3>CODING CHALLENGE</h3>
-<?php
-//connection from the Construct.php
-include_once 'includes/Construct.php';
-
-// ECHO an instance of CAR OBJECT HERE
-echo $bmw->getModel();
-echo "<br>";
-echo $bmw->getColor();
-?>
+    echo $apple->getName();
+    echo "<br>";
+    echo $banana->getName();
+    // Call using browser when the user clicks on something
+    // Call or invoke a funciton inside of our script or code 
+    ?>
 
 
-<hr>
+    <h3>CODING CHALLENGE</h3>
+    <?php
+    //connection from the Construct.php
+    include_once 'includes/Construct.php';
 
-<h2>Visibiliy and Inheritance</h2>
-<?php
-include_once 'includes/Player.php';
-
-$pet01 = new Pet();
-$player01First = new PlayerName();
-
-echo $pet01->owner(); // method owner from the PET class is private -> That is why we are getting a uncaught error > This  will be solved if we set private to public access modifier.
-
-//echo $player01First->petOwner();
- 
-
-?>
-
-<hr>
-<?php 
-echo "<h3>CODING CHALLENGE VISIBILITY AND INHERITANCE</h3>"
-
-// Connect Fruits.php 
+    // ECHO an instance of CAR OBJECT HERE
+    echo $bmw->getModel();
+    echo "<br>";
+    echo $bmw->getColor();
+    ?>
 
 
-// create an object from the class Person.
+    <hr>
 
-// display output for the favorite food of the person.
+    <h2>Visibiliy and Inheritance</h2>
+    <?php
+    include_once 'includes/Player.php';
 
-?>
+    $pet01 = new Pet();
+    $player01First = new PlayerName();
+
+    echo $pet01->owner(); // method owner from the PET class is private -> That is why we are getting a uncaught error > This  will be solved if we set private to public access modifier.
+
+    //echo $player01First->petOwner();
+
+
+    ?>
+
+    <hr>
+    <?php
+    echo "<h3>CODING CHALLENGE VISIBILITY AND INHERITANCE</h3>";
+
+    // Connect Fruits.php 
+    include_once 'includes/Fruit.php';
+
+    // create an object from the class Person.
+    $person = new Person("Eric");
+
+
+    // display output for the favorite food of the person.
+    echo $person->eatFruit();
+    ?>
 
 
 </body>
+
 </html>
