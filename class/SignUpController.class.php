a031e9ec3d0aba1580f85bd80acf325373f31e6e
Rodrigo Lopez <scdtp2019@gmail.com>
2023-04-25 21:22:30 +0800

    Update SignUpController.php

--- class/SignUpController.class.php
+++ class/SignUpController.class.php
@@ -2,6 +2,7 @@
 
     // create a class
 class SignUpController{
+    // PROPERTIES
     // create properties related to data we want to grab
 
     private $uid;
@@ -9,10 +10,24 @@ class SignUpController{
     private $pwdrepeat;
     private $email;
 
+    //METHODS
     // create a function construct
-    public function __construct(){
-        
+    public function __construct($uid, $pwd, $pwdrepeat,$email){
+        $this->uid = $uid;
+        $this->pwd = $pwd;
+        $this->pwdrepeat = $pwdrepeat;
+        $this->email = $email;
     }
+
+    public function emptyInput(){
+        $results;
+        if(empty($this->$uid || $this->$pwd ||$this->$pwdrepeat || $this->$email)){
+
+        }
+    }
+
+    
+
 }
 
 
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,14 +1,22 @@
 <?php 
 
 if(isset($_POST["submit"])){
-    // We want to grab data
+    // We want to grab data from the user input
     $uid = $_POST["uid"];
     $pwd = $_POST["pwd"];
     $pwdrepeat = $_POST["pwdrepeat"];
     $email = $_POST["email"];
 
 
-    // We want to instantiate the data from the SignUpController.class.php
+    // We want to include the data from the SignUpController.class.php
+    
+    include "../class/SignUp.class.php";
+    include "../class/SignUpController.php";
+
+    // Instantiate the SignUpController class
+    $signup = new SignUpController($uid,$pwd,$pwdrepeat,$email);
+
+
 
     // Run some error handlers
 
