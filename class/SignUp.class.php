8e5d3cfa788c3d9892f2c3c96884cd98d05c56c7
Rodrigo Lopez <scdtp2019@gmail.com>
2023-04-26 21:12:44 +0800

    Add Dbh clas and create database table users

---
+++ class/SignUp.class.php
@@ -0,0 +1,20 @@
+<?php
+
+class SignUp extends Dbh{
+    protected function checkUser($uid, $email){
+        $stmt = $this->connect()->prepare('SELECT users_uid FROM users WHERE users_uid = ? or users_email = ?;');
+        if(!$stmt->execute(array($uid,$email))){
+            $stmt = null;
+            header("location:../index.php?error?stmtfailed");
+            exit();
+        }
+        $resultCheck;
+
+        if(!stmt->rowCount() > 0){
+            $resultCheck = false;
+        }else{
+            $resultCheck = true;
+        }
+        return $resultCheck;
+    }
+}
