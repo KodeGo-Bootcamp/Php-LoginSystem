c0e55b73ed149fff3b872e58893da10826660ef6
Rodrigo Lopez <scdtp2019@gmail.com>
2023-04-28 19:33:47 +0800

    Add pseudo code logic for LoginController class

---
+++ class/LoginController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+// Create and declare a LoginController class
+
+// Declare some private properties for the username and password
+
+// Declare a function declaraton for loginUser() 
+
+// Create a function construct to assign property values to our properties
+
+// Check for emptyInput then access a method getUser() to refrence username and password
+
+// Check for emptyInput() 
